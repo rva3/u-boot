62e20549db6dc8cb6643d45a54e7933a95d1dc64
rva3 <rva333@protonmail.com>
2026-06-28 13:33:12 +0300

    configs: add mt6595 config
    Signed-off-by: rva3 <rva333@protonmail.com>
    ---
    internal: remove?

---
+++ include/configs/mt6595.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Configuration for MediaTek MT6595 SoC
+ *
+ */
+
+#ifndef __MT6595_H
+#define __MT6595_H
+
+/* DRAM definition */
+#define CFG_SYS_SDRAM_BASE			0x40000000
+#define CFG_SYS_INIT_RAM_ADDR			0x40000000
+#define CFG_SYS_SDRAM_SIZE			0x80000000
+#define CFG_SYS_INIT_RAM_SIZE			0x80000000
+
+#endif
